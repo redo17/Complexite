a48d51693d92e3eb5a4741afc87ec3d85af1843f
redo17 <redouane.tigrara@ent.univ-amu.fr>
2016-09-09 16:35:14 +0200

    exo3 pour vérification

--- TP1/exo3.c
+++ TP1/exo3.c
@@ -5,37 +5,55 @@
 void main ()
 {
 	int Tab[10];
-	int Max = 0, Idi = 0, Idj = 0;
-	int SousSeqMax,i ,j;
+	int Max = 0,MaxG = 0, MaxD = 0,kmax = 0, lmax = 0,lmax1 = 0,kk = 0,ll = 0,n=10;
+	int SousSeq,SousSeqG,SousSeqD,i ,j, lmaxD;
 	
 	srand(time(NULL));
-	for(i = 0; i < 10; i++)
-		Tab[i] = rand()%20 -10;
+	for(i = 0; i < 10; i++) Tab[i] = rand()%20 -10;
 	
-	for(i = 0; i < 10; i++)
-		printf("%d ",Tab[i]);
+	for(i = 0; i < 10; i++) printf("%d ",Tab[i]);
+
 	printf("\n");
 	
-	for(i = 0; i < 10; i++){
-		SousSeqMax = Tab[i];
-		if(SousSeqMax > Max)
+	for(i = 0; i < (n/2); i++)
+	{
+		SousSeqG = 0; SousSeqD = 0;lmaxD = n;
+
+		for(j = i; j < (n/2) ; j++)
 		{
-			Max = SousSeqMax;
-			Idi = i;
-			Idj = i;
-		}
-		for(j = i+1; j < 10 ; j++)
-		{
-			SousSeqMax += Tab[j];
-			if(SousSeqMax > Max)
+			SousSeqG += Tab[j]; SousSeqD += Tab[n-j-1];lmaxD--;
+			if(SousSeqG > MaxG)
 			{
-				Max = SousSeqMax;
-				Idi = i;
-				Idj = j;
+				MaxG = SousSeqG;
+				kmax = i;
+			}
+			if(SousSeqD > MaxD)
+			{
+				MaxD = SousSeqD;
+				lmax = n-j-1;
+				lmax1 = n-lmaxD-1;				
 			}
 		}
 	}
-	printf("La sous sequence Max est de %d, la borne k est %d, la borne l est %d\n",Max,Idi,Idj);
+	
+	printf("La sous sequence Max Gauche est de %d et Droite est de %d , la borne k est %d, la borne l est %d \n",MaxG,MaxD,kmax+1,lmax+lmax1+1);
+	
+	for(i = kmax; i < lmax+lmax1; i++)
+	{
+		SousSeq = 0;
+
+		for(j = i; j < lmax+lmax1+1 ; j++)
+		{
+			SousSeq += Tab[j];
+			if(SousSeq > Max)
+			{
+				Max = SousSeq;
+				kk = i;
+				ll = j;
+			}
+		}
+	}
+	printf("La sous sequence Max est de %d , la borne k est %d, la borne l est %d\n",Max,kk+1,ll+1);
 }
 	
 	
