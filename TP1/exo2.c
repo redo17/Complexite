b7573355166d43ca033ff0a413755a7d60846c42
Yann Masliah <yann.masliah@etu.univ-amu.fr>
2016-09-19 11:09:20 +0200

    on va tout casser !

--- TP1/exo2.c
+++ TP1/exo2.c
@@ -4,7 +4,11 @@
 
 void main (int argc, char *argv[])
 {
-	int Max = argv[1], Idi = 0, Idj = 0;
+<<<<<<< HEAD
+	int Max = atoi(argv[1]), Idi = 0, Idj = 0;
+=======
+	int Max = atoi(argv[1]), Idi = 1, Idj = 1;
+>>>>>>> b038a41eab56354c90f0d5d501b6e19d3a2a4dc7
 	int SousSeq,i,j;
 	
 	for(i = 1; i < argc; i++){
