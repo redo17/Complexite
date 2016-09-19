7cdebeaf3d80868615328fa8146ab79ac13dc34b
redouane <redouane.tigrara@etu.univ-amu.fr>
2016-09-19 13:05:56 +0200

    MAJ exo2

--- TP1/exo2.c
+++ TP1/exo2.c
@@ -4,11 +4,7 @@
 
 void main (int argc, char *argv[])
 {
-<<<<<<< HEAD
 	int Max = atoi(argv[1]), Idi = 0, Idj = 0;
-=======
-	int Max = atoi(argv[1]), Idi = 1, Idj = 1;
->>>>>>> b038a41eab56354c90f0d5d501b6e19d3a2a4dc7
 	int SousSeq,i,j;
 	
 	for(i = 1; i < argc; i++){
@@ -24,7 +20,7 @@ void main (int argc, char *argv[])
 			}
 		}
 	}
-	printf("La sous sequence Max est de %d, la borne k est %d, la borne l est %d\n",Max,Idi-1,Idj-1);
+	printf("%d %d",Idi-1,Idj-1);
 }
 	
 	
