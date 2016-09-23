d01db7d378034be4033e5c9194652c01f9582ba9
YMasliah <yann.masliah@etu.univ-amu.fr>
2016-09-23 19:46:26 +0200

    renomage d'une variable

--- TP1/exo4.c
+++ TP1/exo4.c
@@ -2,27 +2,24 @@
 #include <stdlib.h>
 #include <math.h>
 
-//PS : Une Sequence Max ne demarrera pas a 0, cela peut fournir une sequence differente des autres fonctions
-//ce qui dit comparaison incorrect dans le script
-
 void main (int argc, char *argv[])
 {
 	int Max = atoi(argv[1]), Idi = 1, Idj = 1;
-	int SousSeq = 0,SousSeqMax = 0,j,i=2;
+	int SousSeq = 0,SousSeqBis = 0,j,i=2;
 	
 	for(j = 2; j < argc; j++){
 		//SousSeq as comme bornes Idj et j
 		SousSeq += atoi(argv[j]);
-		//SousSeqMax additioné a la nouvelle cellule du tableau
-		//SouSeqMax contient la meilleur SousSeq entre Idj et j
-		SousSeqMax += atoi(argv[j]);
+		//SousSeqBis additioné a la nouvelle cellule du tableau
+		//SouSeqBis contient la meilleur SousSeq entre Idj et j
+		SousSeqBis += atoi(argv[j]);
 		
-		//Si la nouvelle cellule est superieur a la SousSeqMax alors
-		//SousSeq max est la nouvelle cellule
-		//(dans le cas où SousSeqMax est/etait negatif)
-		if(atoi(argv[j]) > SousSeqMax)
+		//Si la nouvelle cellule est superieur a la SousSeqBis alors
+		//SousSeqBis est la nouvelle cellule
+		//(dans le cas où SousSeqBis est/etait negatif)
+		if(atoi(argv[j]) > SousSeqBis)
 		{
-			SousSeqMax = atoi(argv[j]);
+			SousSeqBis = atoi(argv[j]);
 			i = j;
 		}		
 		//Si SousSeq est superieur a zero, elle rajoute sa valeur a
@@ -34,14 +31,14 @@ void main (int argc, char *argv[])
 			SousSeq = 0;
 			Idj = j;
 		}
-		//Si SousSeqMax est superieur a la Sequence Max alors
+		//Si SousSeqBis est superieur a la Sequence Max alors
 		//toute les SousSeq sont remise a zero a la recherche d'une valeur
 		//superieur a la valeur negative trouvée
-		if(SousSeqMax > Max)
+		if(SousSeqBis > Max)
 		{
-			Max = SousSeqMax;
+			Max = SousSeqBis;
 			SousSeq = 0;
-			SousSeqMax = 0;
+			SousSeqBis = 0;
 			Idj = j;
 			Idi = i;
 			if(i+1 < argc)
